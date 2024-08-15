6f9d553267d6b8aaaae3eb51d8b024627aa748a2
alexandresouva <pseudoalexandre@gmail.com>
2024-08-15 17:05:59 -0300

    feat: Set the acronym (sigla) as required for generate template

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,16 +2,22 @@ import {
   chain,
   Rule,
   SchematicContext,
+  SchematicsException,
   Tree,
 } from '@angular-devkit/schematics';
 import { buildComponent } from '@angular/cdk/schematics';
-import { Schema } from '@schematics/angular/component/schema';
 import { addImportsToAppModule } from '../utils/template-helper';
 import path = require('path');
+import { IOptions } from '../utils/interfaces';
 
-export function templates(options: Schema): Rule {
+export function templates(options: IOptions): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const appModulePath = path.join(__dirname, 'app.module.ts.template');
+
+    const { name: sigla } = options;
+    if (sigla.length !== 3) {
+      throw new SchematicsException('A sigla deve ter exatamente 3 letras.');
+    }
 
     return chain([
       buildComponent({ ...options, skipImport: true }),
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,4 +1,8 @@
+import { Schema } from '@schematics/angular/component/schema';
+
 export interface IImport {
   classifiedName: string;
   importPath: string;
 }
+
+export interface IOptions extends Schema {}
